6bef07701f744e8d44a3c011cad331b5f665b0b5
Yeprem Karapetyan <eprem96@gmail.com>
2025-06-29 15:04:00 +0400

    some changes

--- passport.js
+++ passport.js
@@ -1,7 +1,6 @@
-// server/passport.js
+require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
 
 // Use the strategy
 passport.use(new GoogleStrategy({
