2c1f1ec24c3237c505adb5f4f1ba14fd7bda47f7
Yeprem Karapetyan <eprem96@gmail.com>
2025-06-29 16:13:33 +0400

    mongo update

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ dotenv.config()
 const app = express();
 
 // ===== Connect to MongoDB =====
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB error:', err));
 
